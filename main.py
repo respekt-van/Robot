580a6912ef8e7fbd3ecc14fd02dc7bb718592cae
respekt-van <66499269+respekt-van@users.noreply.github.com>
2020-11-15 07:13:44 +0300

    Small changes
    Added whole logic of robot (Robot.py). Copied then pasted Neural logic into individual file (Neural.py).

--- main.py
+++ main.py
@@ -1,76 +1,55 @@
-import numpy as np
-import cv2
-
-cap = cv2.VideoCapture(0)
-whT = 320
-confThreshold = 0.5
-nms_threshold = 0.3
-
-classesFile = 'coco.names'
-classNames = []
-with open(classesFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
-#print(classNames)
-#print(len(classNames))
-
-modelConfiguration = 'yolov3-320.cfg'
-modelWeights = 'yolov3-320.weights'
-
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
-def findObjects(outputs, img):
-    hT, wT, cT = img.shape
-    bbox = []
-    classIds = []
-    confs = []
-
-    for output in outputs:
-        for det in output:
-            scores = det[5:]
-            classId = np.argmax(scores)
-            confidence = scores[classId]
-            if confidence > confThreshold:
-                w,h = int(det[2]*wT), int(det[3]*hT)
-                x,y = int((det[0]*wT) -w/2), int(det[1]*hT -h/2)
-                bbox.append([x,y,w,h])
-                classIds.append(classId)
-                confs.append(float(confidence))
-    print(len(bbox))
-    indices = cv2.dnn.NMSBoxes(bbox,confs,confThreshold,nms_threshold)
-
-    for i in indices:
-        i = i[0]
-        box = bbox[i]
-        x,y,w,h = box[0], box[1], box[2], box[3]
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
-        cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%',
-                    (x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,0,255),2)
+from Neural import *
+from Robot import Track
+from Robot import Truck
+from Robot import Manipulator
+IsActive = True
+a = ''
 
 
-
-
-
+# main loop
 while True:
     success, img = cap.read()
 
-    blob = cv2.dnn.blobFromImage(img,1/255,(whT,whT),[0,0,0],1,crop=False)
+    blob = cv2.dnn.blobFromImage(img,1/255,(whT,whT),[0,0,0],1,crop=False)  # ? to blob conv
     net.setInput(blob)
 
     layerNames = net.getLayerNames()
-    #print(layerNames)
     outputNames = [layerNames[i[0]-1] for i in net.getUnconnectedOutLayers()]
-    #print(outputNames)
-    #print(net.getUnconnectedOutLayers())
 
     outputs = net.forward(outputNames)
-    #print(outputs[0].shape)
-    #print(outputs[1].shape)
-    #print(outputs[2].shape)
-    #print(outputs[0][0])
-    findObjects(outputs, img)
-
+    Neural.findObjects(outputs, img)  # Используем нейронку
+    if not Manipulator.busy:  # Если манипулятор
+        # свободен
+        Track.findBottle(Neural.n_ame)  # Ищем бутылку
+        if Track.bottlefound and Truck.IsTurnedToTheTarget() and Neural.BottleRange > 35:  # Если бутыла
+            # найдена, тележка направлена на нёё и расстояние между тележкой и обьектом >35 см
+            Truck.Move(Neural.BottleRange)  # Двигаемся к обьекту
+    if Neural.n_ame == 'BOTTLE' and Neural.BottleRange < 35:  # Если расстояние
+        # меньше 35 см, берем предмет
+        Manipulator.Lock()
+        Manipulator.busy = True
+    if Manipulator.busy:  # Если манипулятор занят
+        Track.findHuman(Neural.n_ame)  # Поиск человека
+    if Manipulator.busy and Track.humanfound and Truck.IsTurnedToTheTarget():
+        Truck.Move(Neural.HumanRange)
+    if Neural.n_ame == 'PERSON' and Neural.BottleRange < 35 and Wannamore.dontwant:
+        Manipulator.Unlock()
+        Manipulator.busy = False
+        while True:
+            print('Робот доставил обьект. Хотите продолжить? (yes или no)')
+            a = input('Введите ответ: ')
+            if a == 'yes' or a == 'YES' or a == 'Yes' or a == 'y' or a == 'Y':
+                IsActive = True
+                Wannamore.dontwant = False
+                break
+            elif a == 'no' or a == 'NO' or a == 'No':
+                IsActive = False
+                break
+            else:
+                print('Некорректный ввод')
+    if not IsActive:
+        break
 
     cv2.imshow('Image', img)
-    cv2.waitKey(1)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
